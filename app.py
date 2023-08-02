afdef76a4b95fca0f2a2becacd5670509e65d309
galileomir <112453386+galileomir@users.noreply.github.com>
2023-08-02 01:11:35 -0700

    changed the post into a new post + get for new ui

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from portfolio import Portfolio
 from scraper import Scraper
 from request_cache import RequestCache
@@ -7,40 +7,39 @@ from stock_factory import StockFactory
 from fund import Fund
 from concurrent.futures import ThreadPoolExecutor
 from flask_cors import CORS
-from flask import jsonify
-
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
+portfolio = Portfolio()
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        symbols = request.form.get('symbols').split(',')
-        shares_list = request.form.get('shares').split(',')
-        shares = [int(share) for share in shares_list]
+@app.route('/receive', methods=['POST'])
+def receive_data():
+    symbols = request.form.get('symbols').split(',')
+    shares_list = request.form.get('shares').split(',')
+    shares = [int(share) for share in shares_list]
 
-        portfolio = Portfolio()
-        request_cache = RequestCache()
+    request_cache = RequestCache()
 
-        with ThreadPoolExecutor() as executor:
-            data_list = list(executor.map(lambda symbol: Scraper(symbol, request_cache).get_data(), symbols))
+    with ThreadPoolExecutor() as executor:
+        data_list = list(executor.map(lambda symbol: Scraper(symbol, request_cache).get_data(), symbols))
 
-        for data, share in zip(data_list, shares):
-            if data["is_fund"]:
-                fund = FundFactory.create(data, request_cache)
-                fund.shares = share
-                process_holding(fund, request_cache, portfolio)
-            else:
-                stock = StockFactory.create(data)
-                stock.shares = share
-                portfolio.add_holding(stock)
+    for data, share in zip(data_list, shares):
+        if data["is_fund"]:
+            fund = FundFactory.create(data, request_cache)
+            fund.shares = share
+            process_holding(fund, request_cache, portfolio)
+        else:
+            stock = StockFactory.create(data)
+            stock.shares = share
+            portfolio.add_holding(stock)
 
-        holdings = portfolio.holdings_table()
-        return jsonify(holdings=holdings)
+    return jsonify({'message': 'Data received successfully'}), 200
 
-    return render_template('index.html')
+@app.route('/return', methods=['GET'])
+def return_data():
+    holdings = portfolio.holdings_table()
+    return jsonify(holdings=holdings), 200
 
 def process_holding(holding, request_cache, portfolio):
     if isinstance(holding, Fund):
@@ -50,5 +49,5 @@ def process_holding(holding, request_cache, portfolio):
     else:
         portfolio.add_holding(holding)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
