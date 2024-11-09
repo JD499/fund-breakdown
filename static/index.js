b4d4b8efd669bd9c26cd19922d9965f72c2ce33a
JD499 <jd499b@gmail.com>
2024-11-09 15:11:37 -0800

    Update Chart.js script source to local file

--- static/index.js
+++ static/index.js
@@ -108,7 +108,7 @@ function displayHoldings(holdings) {
 async function loadChartJS() {
     return new Promise((resolve, reject) => {
         const script = document.createElement('script');
-        script.src = 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js';
+        script.src = '/static/chart.umd.js';
         script.onload = resolve;
         script.onerror = reject;
         document.body.appendChild(script);
